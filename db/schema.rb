5e08032f013aac8417f9033e7215217fb202b240
prkrsign <prkrsign@yahoo.co.jp>
2019-10-09 12:01:26 +0900

    add column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_022526) do
+ActiveRecord::Schema.define(version: 2019_10_09_025154) do
 
   create_table "gears", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "gearname", null: false
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 2019_10_09_022526) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", default: "", null: false
+    t.string "favorite", null: false
+    t.text "profile"
+    t.string "avatar"
+    t.bigint "genre_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
