ddddb2f4ee4b12420f52b18ebbd2d434ed71278e
prkrsign <prkrsign@yahoo.co.jp>
2019-10-09 11:44:44 +0900

    erb to haml and gears#index routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :gears, only: %i(index)
+  root to: 'gears#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
