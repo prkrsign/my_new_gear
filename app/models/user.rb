7c234e4cfc35ccdd5f51b209904d4cf41f4aa6d4
prkrsign <prkrsign@yahoo.co.jp>
2019-10-14 18:47:59 +0900

    add delete and edit button

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :gears
   
   # active_hashでgenreIDを紐づける
   extend ActiveHash::Associations::ActiveRecordExtensions
