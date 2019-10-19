cf581949e613b8fdfbc40eb96e812c5ad709253c
prkrsign <prkrsign@yahoo.co.jp>
2019-10-19 21:15:52 +0900

    add ibanez

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,10 +1,13 @@
 class Scraping
 
+  # hrefのURLを格納する配列を用意
   @@links = []
+  # 共通で使うのでMechanizeを変数化
   @@agent = Mechanize.new
+  # 初回のスクレイピングは、指定したURLであり、その後はリンク先に飛ばしたいので、処理回数nを定義する
   @@n = 0
 
-  # 機材一覧ページから、詳細ページへのリンクを取得する
+  # ブランドごとの機材一覧ページを指定すると、画像ファイルや詳細情報を取ってくる
   def self.boss_urls
     current_page = @@agent.get('https://www.digimart.net/search?brandId=258&category12Id=&nosoldoutp=on')
     page_links(current_page, @@n, @@links)    
@@ -41,16 +44,24 @@ class Scraping
     @@n += 1
   end
 
+  def self.ibanez_urls
+    current_page = @@agent.get('https://www.digimart.net/search?dispMode=ALL&shopNo=&keywordOr=&keywordPhrase=&productName=&categoryId=&categoryNames=&category12Id=1301&category3Id=&brandnames=Ibanez&brandnames=&brandnames=&keywordAnd=&areaId=&priceFrom=&priceTo=&nosoldoutp=on&x=137&y=17&manufactureYearFrom=&manufactureYearTo=&weightOptionFrom=&weightOptionTo=&term=&stringsoption=&pickupOption=&pickupComponentOption=&otherOption=&fretOption=&neckScaleOption=&bodyOption=&tremolantOption=&fingerboardOption=&neckjointOption=&neckOption=&topMaterialOption=&sideMaterialOption=&backMaterialOption=&bodysizeOption=&bodyShapeOption=&materialOption=&specOption=&keywordNot=')
+    page_links(current_page, @@n, @@links)    
+    @@n += 1
+  end
 
 
+  # 一覧ページから詳細ページへのhref属性を抽出して、配列linksに順次格納していく
   def self.page_links(current_page, n, links)
     while true
+      # 初回は指定したURLを読みにいく
       if n == 0
         elements = current_page.search('.itemSearchBoxLeft .pic a')
         elements.each do |ele|
           links << ele.get_attribute('href')
         end
         
+      # 2回目以降は、ページネーションの'次へ'ボタンをクリックした先のURLを読み出しにいく
       else
         next_link = current_page.link_with(text: '次へ').click
         break unless next_link
@@ -60,37 +71,47 @@ class Scraping
           links << ele.get_attribute('href')
         end
       end
+      # クラスメソッドlink_eachを実行
       link_each(links)
     end
   end
 
+  # links配列に入れた詳細ページへのURLを一つ一つ抽出
   def self.link_each(links)
     links.each do |link|
-      sleep(5)
+      # 過度なスプレイピングになるので、5秒ストップ
+      sleep(8)
+      # https://www.digimart.net~~~というURLにアクセスして、get_productの処理を行う
       puts get_product('https://www.digimart.net' + link)
     end
   end
 
+  # link先から、機材名、メーカー、カテゴリー、画像URLを抽出してgearテーブルに保存する
   def self.get_product(link)
-    checks = []
     agent = Mechanize.new
     page = agent.get(link)
     gearname = page.search(".itemDetailBox h1").inner_text if page.at('.itemDetailBox h1')
-    gearname = gearname.gsub(/(BOSS|MXR|Electro-Harmonix|tc electronic|BEHRINGER)|【.+|\(.+|《.+|即.+/, '')
+    gearname = gearname.gsub(/(BOSS|MXR|Electro-Harmonix|tc electronic|BEHRINGER|Ibanez)|【.+|\(.+|《.+|即.+|☆.+|\[.+/, '')
     maker    = page.search(".itemDetail .itemDetailInfo a").inner_text if page.at('.itemDetail .itemDetailInfo a')
     category = page.search('//*[@id="main"]/div/div[1]/ul/li[3]/a').inner_text if page.at('//*[@id="main"]/div/div[1]/ul/li[3]/a')
     image_url = page.at('.mainPhotoBlock img')[:src] if page.at('.mainPhotoBlock img')
     image = 'http:' + image_url
     
+    # メーカー名が存在しない場合は、その他というレコードで保存したいので条件分岐
     if maker.nil?
       maker ='その他'
     end
 
+    # メーカー名が存在しない場合は、その他というレコードで保存したいので条件分岐
     if category.nil?
       category = 'その他'
     end
 
-    gear = Gear.where(gearname: gearname, image: image).first_or_initialize(maker: maker, category: category)
-    gear.save!
+    # first_or_initializeを使って、gearnameとimageでは同じレコードを許さない。makerとcategoryは重複してもかまわない
+    gear = Gear.where(gearname: gearname).first_or_initialize
+    gear.image = image
+    gear.maker = maker
+    gear.category = category
+    gear.save
   end
 end
