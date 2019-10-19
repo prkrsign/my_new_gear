9f32b8cffbe9f2037eb55f4b8084051ae7654670
prkrsign <prkrsign@yahoo.co.jp>
2019-10-19 20:27:55 +0900

    refactoring

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,16 +1,44 @@
 class Scraping
 
-  # 機材一覧ページから、詳細ページへのリンクを取得する
-  def self.gear_urls
-    links = []
-    agent = Mechanize.new
-    next_url = ""
-    n = 0
-    current_page = agent.get('https://www.digimart.net/search?brandId=258&category12Id=&nosoldoutp=on')
-    while true
+  @@links = []
+  @@agent = Mechanize.new
+  @@n = 0
 
+  # 機材一覧ページから、詳細ページへのリンクを取得する
+  def self.boss_urls
+    current_page = @@agent.get('https://www.digimart.net/search?brandId=258&category12Id=&nosoldoutp=on')
+    page_links(current_page, @@n, @@links)    
+    @@n += 1
+  end
+
+  def self.mxr_urls
+    current_page = @@agent.get('https://www.digimart.net/search?brandId=85&category12Id=&nosoldoutp=on')
+    page_links(current_page, @@n, @@links)    
+    @@n += 1
+  end
+
+  def self.harmonix_urls
+    current_page = @@agent.get('https://www.digimart.net/search?brandId=13&category12Id=&nosoldoutp=on')
+    page_links(current_page, @@n, @@links)    
+    @@n += 1
+  end
+
+  def self.tc_urls
+    current_page = @@agent.get('https://www.digimart.net/search?brandId=230&category12Id=&nosoldoutp=on')
+    page_links(current_page, @@n, @@links)    
+    @@n += 1
+  end
+
+  def self.behringer_urls
+    current_page = @@agent.get('https://www.digimart.net/search?brandId=255&category12Id=&nosoldoutp=on')
+    page_links(current_page, @@n, @@links)    
+    @@n += 1
+  end
+
+
+  def self.page_links(current_page, n, links)
+    while true
       if n == 0
-        
         elements = current_page.search('.itemSearchBoxLeft .pic a')
         elements.each do |ele|
           links << ele.get_attribute('href')
@@ -25,22 +53,23 @@ class Scraping
           links << ele.get_attribute('href')
         end
       end
-
-      links.each do |link|
-        puts get_product('https://www.digimart.net' + link)
-      end
-      n += 1
+      link_each(links)
     end
   end
 
+  def self.link_each(links)
+    links.each do |link|
+      sleep(5)
+      puts get_product('https://www.digimart.net' + link)
+    end
+  end
 
   def self.get_product(link)
     checks = []
     agent = Mechanize.new
     page = agent.get(link)
     gearname = page.search(".itemDetailBox h1").inner_text if page.at('.itemDetailBox h1')
-
-    
+    gearname = gearname.gsub(/(BOSS|MXR|Electro-Harmonix|tc electronic|BEHRINGER)|【.+|\(.+|《.+|即.+/, '')
     maker    = page.search(".itemDetail .itemDetailInfo a").inner_text if page.at('.itemDetail .itemDetailInfo a')
     category = page.search('//*[@id="main"]/div/div[1]/ul/li[3]/a').inner_text if page.at('//*[@id="main"]/div/div[1]/ul/li[3]/a')
     image_url = page.at('.mainPhotoBlock img')[:src] if page.at('.mainPhotoBlock img')
