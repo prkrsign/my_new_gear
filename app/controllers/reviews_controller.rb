c2aa62e1c418f338cda2f7f244da1512cd300338
prkrsign <prkrsign@yahoo.co.jp>
2019-10-23 15:05:05 +0900

    add authenticate to review-new

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:edit, :update, :destroy]
   before_action :set_gear,   only: [:new, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new]
 
   def new
     @review = Review.new
