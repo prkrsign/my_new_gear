c1ca381acfc725ea2cdaafaf57163e13640149df
prkrsign <prkrsign@yahoo.co.jp>
2019-11-05 14:52:16 +0900

    add controller

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,2 +1,6 @@
 class SnsCredential < ApplicationRecord
+  def self.find_or_create_from_auth_hash(auth_hash)
+    provider = auth_hash[:provider] #providerはどのサービスで認証したのかを見分けるもの
+    uid = auth_hash[:uid]
+  end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,2 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.credentials.twitter[:api_key], Rails.application.credentials.twitter[:api_secret_key]
 end
