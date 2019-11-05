27340b3715b96223cb2a717d46fb2ca35349b9f1
prkrsign <prkrsign@yahoo.co.jp>
2019-11-05 17:53:14 +0900

    signin finction after signup

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,23 +10,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   # infoで必要な情報を抜き出して、userが存在する場合とuserが存在しない場合で処理を分ける
   def callback_for(provider)
-    # binding.pry
     info = User.find_oauth(request.env["omniauth.auth"])
     @user = info[:user]
     sns = info[:sns]
 
-    # userが存在する場合。sign_inした上でrootにリダレクトされる？
+    # userが存在する場合。sign_inした上でrootにリダレクトされる
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
-
-    # userが存在しない場合。snsから引き抜いた情報を持って、user登録画面に遷移する
-    # user_idとsns_idでリレーションを組む必要があるため、sns_idを次のメソッドに持ち越す。
-    # ユーザー情報はビューに表示する必要があるため、インスタンス変数@userを使う。
     else
-
-      SnsCredential.find_or_create_from_auth_hash(info)
-      redirect_to root_path
+      user_info = SnsCredential.find_or_create_from_auth_hash(info)
+      sign_in user_info
+      redirect_to root_path 
+      set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
     end
   end
 
--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -2,16 +2,22 @@ class SnsCredential < ApplicationRecord
   belongs_to :user
 
   def self.find_or_create_from_auth_hash(auth_hash)
+    # binding.pry
     provider = auth_hash[:sns][:provider] # providerはどのサービスで認証したのかを見分けるもの
     uid = auth_hash[:sns][:uid]
     username = auth_hash[:user][:username]
 
+    # snsの情報を元にユーザーテーブルにアカウントを作成。emailはダミー、パスワードはトークン
     user = User.create(
       username: username,
       email: User.dummy_email(auth_hash),
       password: Devise.friendly_token[0, 20]
     )
 
+    # sns_credentialテーブルにプロバイダーとuid、user_idが重複しないかチェック。重複しないならcreate
     find_or_create_by(provider: provider, uid: uid, user_id: user.id)
+
+    # ログインするためにuserをメソッド呼び出し元に返す
+    return user
   end
 end
