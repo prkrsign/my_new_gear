f0dc88117e833c6d78de25472519bf1c7b143045
prkrsign <prkrsign@yahoo.co.jp>
2019-11-10 19:34:15 +0900

    sns params to registration-page

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,18 +10,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   # infoで必要な情報を抜き出して、userが存在する場合とuserが存在しない場合で処理を分ける
   def callback_for(provider)
+    # binding.pry
     info = User.find_oauth(request.env["omniauth.auth"])
     @user = info[:user]
+    @sns = info[:sns]
 
     # userが存在する場合。sign_inした上でrootにリダレクトされる
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-    else
-      user_info = SnsCredential.find_or_create_from_auth_hash(info)
-      sign_in user_info
-      redirect_to root_path
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    else 
+      render template: 'devise/registrations/new'
     end
-    set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
   end
 
   def failure
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,36 +16,50 @@ class User < ApplicationRecord
   validates :profile,              length: { maximum: 1000 }
   validates :password,             presence: true, length: { minimum: 7, maximum: 20 }, confirmation: true
 
+
+  def self.without_sns_data(auth)
+    user = User.where(email: auth.info.email).first
+    if user.present?
+      sns = SnsCredential.create(
+        uid: auth.uid,
+        provider: auth.provider,
+        user_id: user.id
+      )
+    else
+      user = User.new(
+        username: auth.info.nickname,
+        email: auth.info.email,
+      )
+      sns = SnsCredential.new(
+        uid: auth.uid,
+        provider: auth.provider
+      )
+    end
+    return { user: user ,sns: sns}
+  end
+
+  def self.with_sns_data(auth, snscredential)
+    user = User.where(id: snscredential.user_id).first
+    unless user.present?
+      user = User.new(
+        username: auth.info.username,
+        email: auth.info.email,
+      )
+    end
+    return {user: user}
+  end
+
   def self.find_oauth(auth)
     uid = auth.uid
     provider = auth.provider
-    sns = SnsCredential.where(uid: uid, provider: provider).first
-
-    # sns登録のみ完了してるユーザー?
-    if sns.present?
-      user = User.where(id: sns.user_id).first
-
-    # sns登録 なし
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    if snscredential.present?
+      user = with_sns_data(auth, snscredential)[:user]
+      sns = snscredential
     else
-      # Userテーブル用の情報として、emailとusernameの値をオブジェクト化。値はSNSから引っ張りだす。まだDBには保存しない。
-      user = User.new(
-        email: auth.info.email,
-        username: auth.info.nickname
-      )
-
-      # Snscredentialテーブルにuidとproviderをオブジェクト化。providerはtwitterのこと。uidはprovider側で管理しているidのこと。
-      # UserテーブルのIDと紐づけたいのでこの時点ではまだ保存できない。
-      sns = SnsCredential.new(
-        uid: uid,
-        provider: provider,
-        user_id: nil
-      )
+      user = without_sns_data(auth)[:user]
+      sns = without_sns_data(auth)[:sns]
     end
-    # hashでuser情報と、snsのidを返り値として保持しておく
-    { user: user, sns: sns }
-  end
-
-  def self.dummy_email(auth)
-    "#{auth[:sns][:uid]}-#{auth[:sns][:provider]}@example.com"
+    return { user: user ,sns: sns}
   end
 end
