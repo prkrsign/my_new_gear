6cec6e993369d50a41ba0e994b6a0f78604bc936
prkrsign <prkrsign@yahoo.co.jp>
2019-11-11 21:09:41 +0900

    like create and destroy aciton

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,20 +1,20 @@
 class LikesController < ApplicationController
   # いいね機能一旦停止
-  before_action :set_show, only: %i[create destroy]
+  # before_action :set_review, only: %i[create destroy]
   before_action :set_gear, only: %i[create destroy]
 
   def create
-    @like = current_user.likes.create(gear_id: params[:gear_id])
+    @like = current_user.likes.create(review_id: params[:review_id])
   end
 
   def destroy
-    like = current_user.likes.find_by(gear_id: params[:gear_id])
+    like = current_user.likes.find_by(review_id: params[:review_id])
     like.destroy
   end
 
   private
 
-  def set_show
-    @show = Gear.find(params[:gear_id])
+  def set_gear
+    @gear = Gear.find(params[:gear_id])
   end
 end
