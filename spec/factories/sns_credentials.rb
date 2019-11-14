5f49324b55f577acbd15d04f2433c7671019a059
prkrsign <prkrsign@yahoo.co.jp>
2019-11-14 18:54:03 +0900

    add sns_credential model test

--- spec/factories/sns_credentials.rb
+++ spec/factories/sns_credentials.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :sns_credential do
+    provider              {'twitter'}
+    uid                   { Faker::Number.number(19) }
+    user
   end
 end
---
+++ spec/system/sns_credential_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe 'レビュー機能', type: :system do
+  
+end
